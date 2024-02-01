cc8d5888dacc19e1efabde4c764571d2d2a70438
Manue242 <emmanuelle.elion@mediaschool.me>
2024-02-01 15:58:49 +0100

    Panier_commit1

---
+++ app/src/main/java/com/example/jocene/noSavedItemActivity.kt
@@ -0,0 +1,11 @@
+package com.example.jocene
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class noSavedItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_no_saved_item)
+    }
+}
