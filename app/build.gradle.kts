3f599aa375c48213fb7088a48899498749745a8f
Manue242 <emmanuelle.elion@mediaschool.me>
2024-02-18 17:38:44 +0100

    manuecommit2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,7 +47,9 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-        // Import the Firebase BoM
+
+
+    // Import the Firebase BoM
         implementation(platform("com.google.firebase:firebase-bom:32.7.1"))
 
         // When using the BoM, don't specify versions in Firebase dependencies
